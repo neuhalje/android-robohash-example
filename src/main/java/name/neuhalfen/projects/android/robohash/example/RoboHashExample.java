724988ab3cd234440ce86e8c04e366ddc306b56c
Jens Neuhalfen <jens@neuhalfen.name>
2015-09-24 19:07:35 +0200

    Remove UUID generation from measured time; add synchronous test

--- src/main/java/name/neuhalfen/projects/android/robohash/example/RoboHashExample.java
+++ src/main/java/name/neuhalfen/projects/android/robohash/example/RoboHashExample.java
@@ -1,6 +1,7 @@
 package name.neuhalfen.projects.android.robohash.example;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -57,37 +58,87 @@ public class RoboHashExample extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_speed_test) {
-            new AsyncTask<Void, Void, String>() {
-                @Override
-                protected void onPostExecute(String result) {
-
-                    Toast.makeText(RoboHashExample.this, result, Toast.LENGTH_LONG).show();
-                }
-
-                @Override
-                protected String doInBackground(Void... params) {
-                    long startTime = System.currentTimeMillis();
-
-                    final int SAMPLES = 500;
-                    for (int i = 0; i < SAMPLES; i++) {
-                        UUID uuid = UUID.randomUUID();
-                        Handle handle = robots.calculateHandleFromUUID(uuid);
-                        try {
-                            Bitmap bitmap = robots.imageForHandle(handle);
-                        } catch (IOException e) {
-                            //
-                        }
-                    }
-
-                    long endTime = System.currentTimeMillis();
-
-                    long duration = endTime - startTime;
-                    return "Loading " + SAMPLES + " images took " + duration + " ms (" + duration / SAMPLES + " ms/robot)";
-                }
-            }.execute();
+        if (item.getItemId() == R.id.menu_speed_test_asnc) {
+            testLoadingInTheBackground();
+            return true;
+        } else if (item.getItemId() == R.id.menu_speed_test_sync) {
+            testLoadingWithRenderingInTheUiThread();
             return true;
         } else
             return super.onOptionsItemSelected(item);
+    }
+
+    private void testLoadingInTheBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected void onPostExecute(String result) {
+                new AlertDialog.Builder(RoboHashExample.this).setMessage(result).create().show();
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+
+                final int SAMPLES = 500;
+
+                UUID[] seeds = new UUID[SAMPLES];
+                for (int i = 0; i < SAMPLES; i++) {
+                    seeds[i] = UUID.randomUUID();
+                }
+
+                long startTimeCompose = System.currentTimeMillis();
+
+                for (int i = 0; i < SAMPLES; i++) {
+                    Handle handle = robots.calculateHandleFromUUID(seeds[i]);
+                    try {
+                        Bitmap bitmap = robots.imageForHandle(handle);
+                    } catch (IOException e) {
+                        //
+                    }
+                }
+
+                long durationCompose = System.currentTimeMillis() - startTimeCompose;
+                return "Loading " + SAMPLES + " images took " + durationCompose + " ms total "
+                        + " (loading took " + durationCompose / SAMPLES + " ms/robot)";
+            }
+        }.execute();
+    }
+
+
+    void testLoadingWithRenderingInTheUiThread() {
+        final int SAMPLES = 100;
+
+        // How log does writing to the image control take?
+        long sumRenderingIntoImage = 0;
+
+
+        UUID[] seeds = new UUID[SAMPLES];
+        for (int i = 0; i < SAMPLES; i++) {
+            seeds[i] = UUID.randomUUID();
+        }
+
+        long startTimeCompose = System.currentTimeMillis();
+
+        for (int i = 0; i < SAMPLES; i++) {
+            Handle handle = robots.calculateHandleFromUUID(seeds[i]);
+            try {
+                Bitmap bitmap = robots.imageForHandle(handle);
+
+                long startRender = System.currentTimeMillis();
+                RoboHashExample.this.robot.setImageBitmap(bitmap);
+                sumRenderingIntoImage += (System.currentTimeMillis() - startRender);
+            } catch (IOException e) {
+                //
+            }
+        }
+
+
+        long durationCompose = System.currentTimeMillis() - startTimeCompose;
+        String msg = "Loading + rendering " + SAMPLES + " images took " + durationCompose + " ms total "
+                + (durationCompose - sumRenderingIntoImage) + " ms for loading and "
+                + sumRenderingIntoImage + " ms for rendering " +
+                " (loading +  rendering: " + durationCompose / SAMPLES + " ms/robot)";
+
+        new AlertDialog.Builder(this).setMessage(msg).create().show();
+
     }
 }
